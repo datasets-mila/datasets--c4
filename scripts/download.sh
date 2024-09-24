394f9a0b7376b9d789354f3e76137335db7353ec
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2024-09-24 16:54:43 -0400

    Update download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -o errexit -o pipefail -o noclobber
+source scripts/utils.sh echo -n
+
+# Saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail
 
 # This script is meant to be used with the command 'datalad run'
 
@@ -8,15 +11,34 @@ pushd bin/ >/dev/null
 export PATH="${PATH}:${PWD}"
 popd >/dev/null
 
+# Download dataset
 pushd c4/ >/dev/null
 
-git lfs install --skip-smudge
-# git lfs track "*.wav"
-# git add .gitattributes
-
+git lfs install --local --skip-smudge
 git lfs fetch
 git lfs pull
 
 popd >/dev/null
 
-./scripts/stats.sh c4/*/
+# Compile stats
+# this is to have an idea of the internal state of the dataset
+for d in c4/
+do
+	printf "%s\n" "${d}"
+done | sort -u | ./scripts/stats.sh
+
+git-annex add --fast -c annex.largefiles=nothing *.stats
+
+# Verify dataset
+if [[ -f md5sums ]]
+then
+	md5sum -c md5sums
+fi
+list -- --fast | while read f
+do
+	if [[ -z "$(echo "${f}" | grep -E "^bin/")" ]] &&
+		[[ -z "$(grep -E " (\./)?${f//\./\\.}$" md5sums)" ]]
+	then
+		md5sum "${f}" >> md5sums
+	fi
+done
